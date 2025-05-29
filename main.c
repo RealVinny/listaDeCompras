6a32db0c056d18257a79beec7f9aa2060fb7f36d
RealVinny <viniciusrafael2435@gmail.com>
2025-05-29 14:51:42 -0300

    funções de remoção já criadas, falta arrumar a funcionalidade

--- main.c
+++ main.c
@@ -1,55 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
+
+
+    
+
+void addItem(char * list[], int *index, char *element){
+
+    list[*index] = strdup(element);
+    (*index)++;
+   
+ 
+}
+
+void RemoveItem(char * list[], char *element){
+
+    for(int i = 0; sizeof(*list) > i; i++){
+        
+        if(strcasecmp(list[i], element)){
+            strcpy(list[i], NULL);
+            printf("elemento removido com sucesso");
+            break;
+        }
+
+    }
+}
+
+
+void ListItems(char *list[]){
+
+    for(int i = 0; sizeof(*list) > i; i++){
+        printf("%s", list[i]);
+    }
+}
 
 
 
-#define tamanhoMaxArray  15
-#define tamanhoMaxNome  40
 
 int main(){
 
-	char lista[tamanhoMaxArray][tamanhoMaxNome] = {};
-	int tamanho = 0;
+char * lista[50] = {0};
+int quantItens = {0};
+int * pQuantItens = &quantItens;
+char item[] = "";
+char op[4] = "";
 
-	void addItem(char shoppingList[][tamanhoMaxNome], int *tamanho, int itemMax, const char *itm){
-	
-		if(*tamanho < tamanhoMaxArray){
-			strncpy(shoppingList[*tamanho], itm, 40);
-			shoppingList[*tamanho][39] = '\0';
-			(*tamanho)++;
-		}else{
-			printf("lista esta cheia, nao foi possivel adicionar o item!");
-	
-		}
-	}
+printf("oque você deseja fazer?\n");
+printf("[A]dicionar, [R]emover, [L]istar\n ->");
+scanf("%c", op);
+
+    if(strcasecmp("A", op)){ // Adicionando Itens
+    
+
+        while( 1 == 1){
+
+    char entrada[] = {0};
+    
+
+    printf("diga o nome do item para ser adicionado:");
+    scanf("%s", entrada);
+
+        if(strcasecmp(entrada, "quit")){
+            break;
+        }
+
+    addItem(lista, pQuantItens, entrada);
+
+        
+
+    }
+    
+        }else if(strcasecmp("R", op)){ // Removendo item
+
+    char entrada[] = {0};
 
 
-	void removeItem(char shoppingList[][tamanhoMaxNome], int *tamanho, int index){
-	
-		if( index < 0 || index >= *tamanho){
-		
-		printf("index invalido!");
-		return;
+        while( 1 == 1){
 
-		}
+    char entrada[] = {0};
+    
 
-		for(int i = index; i < *tamanho - 1; i++){
-				strcpy(shoppingList[i], shoppingList[i-1]);
-		}
+    printf("diga o nome do item para ser removido:");
+    scanf("%s", entrada);
 
-		memset(shoppingList[*tamanho -1], 0, tamanhoMaxNome);
+        if(strcasecmp(entrada, "quit")){
+            break;
+            } 
+  
+            RemoveItem(lista, entrada);
 
-		(*tamanho)--;
-	}
-	
+        
+
+    }
+
+        }else if(strcasecmp("L", op)){ // Listando Itens
+
+    char entrada[] = {0};
+
+
+        while( 1 == 1){
+
+    char entrada[] = {0};
+    
+
+    printf("diga o nome do item para ser adicionado:");
+    scanf("%s", entrada);
+
+        if(strcasecmp(entrada, "quit")){
+            break;
+
+        ListItems(lista);
 
 
 
-	addItem(lista, &tamanho, tamanhoMaxArray, "agua");
-	printf("%s", lista[0]);
-	removeItem(lista, &tamanho, 0);
-	printf("%s", lista[0]);
+    }
+
+    }
+    }
+
+printf("%s", lista[0]);
+
+
 
 	return 0;
 }
